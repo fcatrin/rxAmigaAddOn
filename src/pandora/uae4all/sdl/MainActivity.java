ce75b3e4e5758ac4fd011c8fe13b3d8c96d65794
Franco Catrin <fcatrin@gmail.com>
2014-07-28 17:47:29 +0000

    Refactor para eliminar codigo obsoleto de Overlay

--- src/pandora/uae4all/sdl/MainActivity.java
+++ src/pandora/uae4all/sdl/MainActivity.java
@@ -46,7 +46,6 @@ import retrobox.vinput.overlay.ExtraButtonsController;
 import retrobox.vinput.overlay.ExtraButtonsView;
 import retrobox.vinput.overlay.GamepadController;
 import retrobox.vinput.overlay.GamepadView;
-import retrobox.vinput.overlay.Overlay;
 import retrobox.vinput.overlay.OverlayNew;
 import android.app.Activity;
 import android.app.Notification;
@@ -1332,7 +1331,7 @@ public class MainActivity extends Activity
         menu.add(0, CANCEL_ID, 0, "Cancel");
         menu.add(0, LOAD_ID, 0, R.string.load_state);
         menu.add(0, SAVE_ID, 0, R.string.save_state);
-        if (Overlay.hasExtraButtons()) {
+        if (OverlayNew.hasExtraButtons()) {
         	menu.add(0, BUTTONS_ID, 0, "Extra Buttons");
         }
         if (needsOverlay()) {
