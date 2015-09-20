76cb37366aa2d87a1791792d3cf99e23de672f63
Franco Catrin <fcatrin@gmail.com>
2015-09-20 17:26:00 +0000

    Adds x86 build

--- jni/application/uae4all2/AndroidBuild.sh
+++ jni/application/uae4all2/AndroidBuild.sh
@@ -21,9 +21,9 @@ fi
 #if [ "$1" = armeabi-v7a ]; then
 #../setEnvironment-armeabi-v7a.sh sh -c "make --makefile=Makefile.arm-v7a-neon -j1" && mv -f uae4all-v7a libapplication-armeabi-v7a.so
 #fi
-#if [ "$1" = x86 ]; then
-#../setEnvironment-x86.sh sh -c "make --makefile=Makefile.x86 -j1" && mv -f uae4all-x86 libapplication-x86.so
-#fi
+if [ "$1" = x86 ]; then
+../setEnvironment-x86.sh sh -c "make --makefile=Makefile.x86 -j1" && mv -f uae4all-x86 ../../../libs/x86/libapplication-x86.so
+fi
 #if [ "$1" = mips ]; then
 #../setEnvironment-mips.sh sh -c "make --makefile=Makefile.mips -j1" && mv -f uae4all-mips libapplication-mips.so
 #fi
