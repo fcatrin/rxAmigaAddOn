4aceef1c0429462fe9c3137a1554affa2ac183ba
Franco Catrin <fcatrin@gmail.com>
2015-11-04 00:19:41 +0000

    Agrega immersive mode compatible con pre-kitkat

--- src/pandora/uae4all/sdl/MainActivity.java
+++ src/pandora/uae4all/sdl/MainActivity.java
@@ -36,6 +36,7 @@ import java.util.zip.CheckedInputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
+import retrobox.utils.ImmersiveModeSetter;
 import retrobox.vinput.GenericGamepad.Analog;
 import retrobox.vinput.Mapper;
 import retrobox.vinput.QuitHandler;
@@ -300,19 +301,12 @@ public class MainActivity extends Activity
 		}
 	}
 
-	@TargetApi(Build.VERSION_CODES.KITKAT)
 	private void setImmersiveMode() {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-			getWindow().getDecorView().setSystemUiVisibility(
-		            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-		            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-		            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-		            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-		            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-		            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-		} else {
-			
-		}
+		ImmersiveModeSetter.get().setImmersiveMode(getWindow(), isStableLayout());
+	}
+	
+	private boolean isStableLayout() {
+		return Mapper.hasGamepads();
 	}
 	
 	public void startDownloader()
@@ -459,11 +453,11 @@ public class MainActivity extends Activity
 	@Override
 	protected void onResume() {
 		super.onResume();
+		ImmersiveModeSetter.postImmersiveMode(new Handler(), getWindow(), isStableLayout());
+		
 		if( mGLView != null )
 		{
 			mGLView.onResume();
-			DimSystemStatusBar.get().dim(_videoLayout);
-			DimSystemStatusBar.get().dim(mGLView);
 		}
 		else
 		if( downloader != null )
